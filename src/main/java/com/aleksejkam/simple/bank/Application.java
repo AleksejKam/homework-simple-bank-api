2b37bc06be628fff7ff9800720aac694d6883362
Aleksej Kaminskij <liohak@gmail.com>
2020-08-01 13:55:05 +0300

    feat: Add Application and Security Config

---
+++ src/main/java/com/aleksejkam/simple/bank/Application.java
@@ -0,0 +1,17 @@
+package com.aleksejkam.simple.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application
+ *
+ * @author Aleksej Kaminskij
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
