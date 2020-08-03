6946c3166ba0965fd57a1c924ce241bc5277480b
Aleksej Kaminskij <liohak@gmail.com>
2020-08-04 00:38:47 +0300

    feat: Create main methods in AccountController and removed temporary methods

--- src/main/java/com/aleksejkam/simple/bank/controller/AccountController.java
+++ src/main/java/com/aleksejkam/simple/bank/controller/AccountController.java
@@ -1,8 +1,15 @@
 package com.aleksejkam.simple.bank.controller;
 
 import com.aleksejkam.simple.bank.model.Account;
+import com.aleksejkam.simple.bank.model.Transaction;
+import com.aleksejkam.simple.bank.domain.TransactionRequest;
 import com.aleksejkam.simple.bank.service.AccountService;
+import com.aleksejkam.simple.bank.service.TransactionService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,26 +24,59 @@ import java.util.Optional;
  * @author Aleksej Kaminskij
  */
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/v1/account")
 public class AccountController {
 
     @Autowired
     private AccountService accountService;
 
-    @PostMapping("/add")
-    public Account createAccount(@RequestBody Account account) {
-        account.setUsername(getUserName().get());
+    @Autowired
+    private TransactionService transactionService;
 
-        return accountService.saveAccount(account);
+    @GetMapping
+    public ResponseEntity<Account> getAccount() throws NotFoundException {
+        Account account = accountService.findAccountByUsername(getAuthenticatedUserName().get());
+
+        return new ResponseEntity<>(account, new HttpHeaders(), HttpStatus.OK);
     }
 
-    @GetMapping("/get-all")
-    public List<Account> getAllAccount() {
-        return accountService.findAllAccount();
+    @PostMapping("/deposit")
+    public ResponseEntity<String> makeDeposit(@RequestBody TransactionRequest transactionRequest) throws NotFoundException {
+        Account account = accountService.findAccountByUsername(getAuthenticatedUserName().get());
+
+        accountService.makeDeposit(account, transactionRequest);
+
+        return ResponseEntity.ok("Deposit is done");
     }
 
-    private Optional<String> getUserName() {
+    @PostMapping("/withdraw")
+    public ResponseEntity<String> makeWithdraw(@RequestBody TransactionRequest transactionRequest) throws NotFoundException {
+        Account account = accountService.findAccountByUsername(getAuthenticatedUserName().get());
 
+        accountService.makeWithdraw(account, transactionRequest);
+
+        return ResponseEntity.ok("withdraw is done");
+    }
+
+    @GetMapping("/transactions")
+    public ResponseEntity<List<Transaction>> getTransactions() throws NotFoundException {
+        Account account = accountService.findAccountByUsername(getAuthenticatedUserName().get());
+
+        return ResponseEntity.ok(transactionService.getTransactionsByAccountId(account.getId()));
+    }
+
+    @GetMapping("/transactions/{transactionId}")
+    public ResponseEntity<Transaction> getTransaction(@PathVariable Long transactionId) throws NotFoundException {
+        Transaction transaction = transactionService.getTransactionById(transactionId);
+
+        if (transaction == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(transaction);
+    }
+
+    private Optional<String> getAuthenticatedUserName() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
@@ -44,6 +84,5 @@ public class AccountController {
         } else {
             return Optional.empty();
         }
-
     }
 }
